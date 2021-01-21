fe20a87b1d3441b411a919592e8a7ef8f2f6175f
alumne <alumne@alumnepc>
2021-01-21 11:57:19 +0100

    tete

--- Calculadora/src/com/alumne/calculadora/Calculadora.java
+++ Calculadora/src/com/alumne/calculadora/Calculadora.java
@@ -12,7 +12,7 @@ import org.eclipse.swt.widgets.Text;
 
 public class Calculadora {
 
-	// Constantstt
+	// Constantst
 	final int MAX_DIGITS = 5;
 	final int MODE_ENTRADA = 0;
 	final int MODE_RESULTAT = 1;
